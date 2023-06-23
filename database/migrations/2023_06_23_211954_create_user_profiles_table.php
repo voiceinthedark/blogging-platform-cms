25330ce8be1cfa2480ff921d3f8ec7ce3b874566
voiceinthedark <darkrisingforce@gmail.com>
2023-06-24 01:11:08 +0300

    Added Jetstream Authetication; Added UserProfile, Post models and migrations

---
+++ database/migrations/2023_06_23_211954_create_user_profiles_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->longText('bio')->nullable();
+            $table->unsignedBigInteger('page_visits_count');
+            $table->string('profile_photo_path', 2048)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_profiles');
+    }
+};
