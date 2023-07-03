30c4b204874e20a7f62708e6211747c7aff22c08
voiceinthedark <darkrisingforce@gmail.com>
2023-07-03 17:13:04 +0300

    Added replies to comments functionality

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Comment extends Model
 {
@@ -31,5 +32,10 @@ class Comment extends Model
     public function post(): BelongsTo
     {
         return $this->belongsTo(Post::class);
+    }
+
+    // May have many replies
+    public function replies() : HasMany{
+        return $this->hasMany(Comment::class, 'parent_id', 'id');
     }
 }
--- resources/views/livewire/comments/show-comments.blade.php
+++ resources/views/livewire/comments/show-comments.blade.php
@@ -8,7 +8,7 @@
 
     </div>
     @foreach ($post->comments as $comment)
-        <div x-data="{commentShow: false}">
+        <div x-data="{commentShow: false, replyShow: false}">
             <div class="flex flex-col items-start">
                 <span class="font-bold">{{ $comment->user->name }}</span>
                 <span class="text-gray-500">{{ $comment->created_at->diffForHumans() }}</span>
@@ -19,6 +19,25 @@
             <div class="flex flex-row justify-end">
                 <button class="text-blue-600" @click="commentShow = !commentShow" >Reply</button>
             </div>
+            <!-- If Comment has replies -->
+            @if (!empty($comment->replies))
+                @if (count($comment->replies) > 0)
+                <button type="button" class="mb-2 text-blue-600 hover:text-blue-400 transition-colors" x-on:click="replyShow = !replyShow">show {{ count($comment->replies) }} replies</button>
+                @endif
+                <div x-show="replyShow">
+                    @foreach ($comment->replies as $reply)
+                        <div class="ml-16">
+                            <div class="flex flex-col items-start">
+                                <span class="font-bold">{{ $reply->user->name }}</span>
+                                <span class="text-gray-500">{{ $reply->created_at->diffForHumans() }}</span>
+                            </div>
+                            <div class="p-4 bg-gray-100 m-2 text-secondary-800 border rounded border-l-slate-700">
+                                {{ $reply->content }}
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            @endif
             <div class="mb-4 mt-2 ml-12" x-show="commentShow" wire:target="comment" x-transition:enter="transition ease-in duration-200" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"  x-transition:leave="transition ease-out duration-200"
              wire:ignore>
                 <x-textarea-wireui id="comment-{{$comment->id}}" name="comment" label="{{ auth()->user()->name}}" wire:model="comment">
