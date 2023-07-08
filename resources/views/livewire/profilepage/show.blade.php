e52770e858c411dec4e95c44ac6895cb63aa4987
voiceinthedark <darkrisingforce@gmail.com>
2023-07-08 04:12:48 +0300

    Added Qr code to user profile add links to social

--- resources/views/livewire/profilepage/show.blade.php
+++ resources/views/livewire/profilepage/show.blade.php
@@ -5,25 +5,31 @@
         <div class="text-lg mb-5">{{ '@' . $user->userprofile->username }}</div>
         <div class="flex items-center">
             <img src="{{ asset('storage/logo/icons8-twitter.svg') }}" class="h-8 w-8" alt="twitter-icon" />
-            <span class="text-lg">{{ $user->userProfile->twitter }}</span>
+            <a href="https://twitter.com/{{ $user->userProfile->twitter }}"><span class="text-lg text-sky-700">{{ '@' . $user->userProfile->twitter }}</span></a>
         </div>
         <div class="flex items-center">
             <img class="h-8 w-8" src="{{ asset('storage/logo/icons8-instagram.svg') }}" alt="instagram-icon" />
-            <span class="text-lg">{{ $user->userProfile->instagram }}</span>
+            <a href="https://instagram.com/{{ $user->userProfile->instagram }}"><span class="text-lg text-sky-700">{{ '@' . $user->userProfile->instagram }}</span></a>
         </div>
         <div class="flex items-center">
             <img class="h-8 w-8" src="{{ asset('storage/logo/icons8-facebook.svg') }}" alt="facebook-icon" />
-            <span class="text-lg">{{ $user->userProfile->facebook }}</span>
+            <a href="https://facebook.com/{{ $user->userProfile->facebook }}"><span class="text-lg text-sky-700">{{ '@' . $user->userProfile->facebook }}</span></a>
         </div>
         <div class="flex items-center">
             <img class="h-8 w-8" src="{{ asset('storage/logo/icons8-github.svg') }}" alt="github-icon" />
-            <span class="text-lg">{{ $user->userProfile->github }}</span>
+            <a href="https://github.com/{{ $user->userProfile->github }}"><span class="text-lg text-sky-700">{{ '@' . $user->userProfile->github }}</span></a>
         </div>
         <div class="text-lg mt-5 p-4 shadow-sm rounded-sm border border-dashed border-gray-400 bg-gray-100">
             {{ $user->userProfile->bio }}
         </div>
+        <!-- User QR Code -->
+        <div class="mt-4">
+            {!! QrCode::generate(
+                $user->email
+                ) !!}
+        </div>
     </div>
-    <div class="flex flex-col justify-start items-center mt-20">
+    <div class="flex flex-col justify-start items-center mx-4 mt-20">
         <span
             class="text-2xl font-semibold mb-5">{{ $user->name . ' has ' . $user->posts->count() . ' articles' }}</span>
         <livewire:profilepage.show-user-articles :user="$user" :posts="$user->posts" />
